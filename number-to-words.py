08ca8f1c404173376269674a8880ff622cbc94a4
Wilfredo D. Catalan <catalanwilfredo97@gmail.com>
2023-12-13 00:17:00 +0800

    second commit

--- number-to-words.py
+++ number-to-words.py
@@ -1,11 +1,19 @@
 from tkinter import *
 from datetime import datetime
 from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import Scrollbar
+from tkinter import Menu
 
 root = Tk()
 root.title("Number to Words")
 root.geometry('1200x700+10+100')
 root.resizable(False, False)
+
+def copy_to_clipboard():
+    root.clipboard_clear()
+    root.clipboard_append(t1.get("1.0", END))
+    root.update()
 
 
 def show():
@@ -26,7 +34,14 @@ def show():
 
 def clear():
     e1.delete(0, END)
-    # t1.delete(1.0, END)
+    t1.delete(1.0, END)
+
+def save_to_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(t1.get("1.0", END))
+        messagebox.showinfo("Info", "Conversion history saved successfully!")
 
 num = StringVar()
 
@@ -73,6 +88,13 @@ def convert_to_words(number):
 
     return result.strip()
 
+menu_bar = Menu(root)
+root.config(menu=menu_bar)
+
+edit_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Copy", command=copy_to_clipboard)
+
 Label(root,text="Enter a Number:", font=('helvetica 12 bold')).place(x=10,y=10)
 Label(root,text="Datetime:", font=('helvetica 10 bold')).place(x=5,y=85)
 Label(root,text="Number:", font=('helvetica 10 bold')).place(x=220,y=85)
@@ -82,7 +104,13 @@ e1.place(x=150,y=10)
 e1.focus()
 Button(root, text='Submit', font=('helvetica 10 bold'), width=10, command=show).place(x=150,y=50)
 Button(root, text='Clear', font=('helvetica 10 bold'), width=10, command=clear).place(x=260,y=50)
+Button(root, text='Save to File', font=('helvetica 10 bold'), width=15, command=save_to_file).place(x=370, y=50)
 t1 = Text(root, width=132, height=30,font=('sans-serif 12 bold'))
 t1.place(x=5,y=110)
+scrollbar = Scrollbar(root, command=t1.yview)
+scrollbar.place(x=1180, y=110, height=575)
+
+t1.configure(yscrollcommand=scrollbar.set)
+
 
 root.mainloop()
